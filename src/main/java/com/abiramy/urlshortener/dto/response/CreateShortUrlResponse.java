684e43141c5031859d6c3fd2baf2fa11525ae180
ThiyakuAbi20 <abiramythiyakesan280@gmail.com>
2026-08-06 17:54:30 +0530

    feat: create CreatShortUrlResponse

---
+++ src/main/java/com/abiramy/urlshortener/dto/response/CreateShortUrlResponse.java
@@ -0,0 +1,66 @@
+package com.abiramy.urlshortener.dto.response;
+
+public class CreateShortUrlResponse {
+
+    private Long id;
+
+    private String originalUrl;
+
+    private String shortCode;
+
+    private String shortUrl;
+
+    private String message;
+
+    public CreateShortUrlResponse(){
+
+    }
+
+    public CreateShortUrlResponse(Long id, String originalUrl, String shortCode, String shortUrl, String message) {
+        this.id = id;
+        this.originalUrl = originalUrl;
+        this.shortCode = shortCode;
+        this.shortUrl = shortUrl;
+        this.message = message;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getOriginalUrl() {
+        return originalUrl;
+    }
+
+    public void setOriginalUrl(String originalUrl) {
+        this.originalUrl = originalUrl;
+    }
+
+    public String getShortUrl() {
+        return shortUrl;
+    }
+
+    public void setShortUrl(String shortUrl) {
+        this.shortUrl = shortUrl;
+    }
+
+    public String getShortCode() {
+        return shortCode;
+    }
+
+    public void setShortCode(String shortCode) {
+        this.shortCode = shortCode;
+    }
+}
--- src/main/java/com/abiramy/urlshortener/service/ShortUrlService.java
+++ src/main/java/com/abiramy/urlshortener/service/ShortUrlService.java
@@ -1,4 +1,37 @@
 package com.abiramy.urlshortener.service;
 
+import com.abiramy.urlshortener.repository.ShortUrlRepository;
+import java.util.Random;
+
 public class ShortUrlService {
+
+    private final ShortUrlRepository shortUrlRepository;
+    private static final String CHARACTERS =
+            "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    private static final int SHORT_CODE_LENGTH = 6;
+
+
+    public ShortUrlService (ShortUrlRepository shortUrlRepository){
+        this.shortUrlRepository = shortUrlRepository;
+    }
+
+    private String generateShortCode(){
+
+        Random random = new Random();
+
+        StringBuilder shortCode = new StringBuilder();
+
+        for(int i = 0; i < SHORT_CODE_LENGTH; i++){
+
+            int randomIndex = random.nextInt(CHARACTERS.length());
+
+            shortCode.append(
+                    CHARACTERS.charAt(randomIndex)
+            );
+        }
+
+        return shortCode.toString();
+    }
+
+
 }
