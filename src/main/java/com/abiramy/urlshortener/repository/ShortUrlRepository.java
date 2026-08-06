8c840c987fd3a5f1fbf4d155e2af3db49d9405e8
ThiyakuAbi20 <abiramythiyakesan280@gmail.com>
2026-08-06 17:12:13 +0530

    feat: create ShortUrlRepository

--- src/main/java/com/abiramy/urlshortener/repository/ShortUrlRepository.java
+++ src/main/java/com/abiramy/urlshortener/repository/ShortUrlRepository.java
@@ -3,7 +3,9 @@ package com.abiramy.urlshortener.repository;
 import com.abiramy.urlshortener.entity.ShortUrl;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ShortUrlRepository extends JpaRepository<ShortUrl, Long> {
 
-
+    Optional<ShortUrl> findByShortCode(String shortCode);
 }
---
+++ src/main/java/com/abiramy/urlshortener/service/ShortUrlService.java
@@ -0,0 +1,4 @@
+package com.abiramy.urlshortener.service;
+
+public class ShortUrlService {
+}
